cdc8bba8059bec0c0ecbded6efa80d7b42dd7fe8
mmkm <mmkm_18@yahoo.com>
2014-08-26 17:21:15 +0600

    Day28(26-08-14)

---
+++ PartialViewPassingModel/PartialViewPassingModel/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewPassingModel.Models;
+
+namespace PartialViewPassingModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Product p1 = new Product() { Name = "Ice cream", Price = 250 };
+            Product p2 = new Product() { Name = "Potato", Price = 20 };
+            Product p3 = new Product() { Name = "Burger", Price = 300 };
+
+            List<Product> productList = new List<Product>();
+            productList.Add(p1);
+            productList.Add(p2);
+            productList.Add(p3);            
+            return View(productList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PartialViewPassingModel/PartialViewPassingModel/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewPassingModel.Models;
+
+
+namespace PartialViewPassingModel.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductDbContext productDbContext = new ProductDbContext();
+
+        public ActionResult Index()
+        {
+            var products = productDbContext.Products;
+            return View(products.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product aProduct)
+        {
+            productDbContext.Products.Add(aProduct);
+            productDbContext.SaveChanges();
+            return View();
+        }
+    }
+}
